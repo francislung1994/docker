13ba06a31f002160e927129445a106837ef1fc7e
francislung1994 <francislung1994@gmail.com>
2025-07-24 22:16:32 +0800

    hello jenkins

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ let http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello Jenkins!');
 }).listen(3000);
